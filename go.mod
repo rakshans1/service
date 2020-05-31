548d5ac612ca0435701712c2d3f6a88828be1b79
Rakshan Shetty <shetty.raxx555@gmail.com>
2020-05-31 17:15:52 +0530

    Parsing config

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/rakshans1/service
 
 go 1.12
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1 // indirect
+	github.com/GuiaBolso/darwin v0.0.0-20191218124601-fd6d2aa3d244
+	github.com/ardanlabs/conf v1.3.2
+	github.com/cznic/ql v1.2.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.6.0
+	github.com/pkg/errors v0.9.1
+	google.golang.org/appengine v1.6.6 // indirect
+)
---
+++ internal/platform/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"net/url"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// Config is the required properties to use the database.
+type Config struct {
+	User       string
+	Password   string
+	Host       string
+	Name       string
+	DisableTLS bool
+}
+
+// Open knows how to open a database connection.
+func Open(cfg Config) (*sqlx.DB, error) {
+	// Define SSL mode.
+	sslMode := "require"
+	if cfg.DisableTLS {
+		sslMode = "disable"
+	}
+	// Query parameters.
+	q := make(url.Values)
+	q.Set("sslmode", sslMode)
+	q.Set("timezone", "utc")
+
+	// Construct url.
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword(cfg.User, cfg.Password),
+		Host:     cfg.Host,
+		Path:     cfg.Name,
+		RawQuery: q.Encode(),
+	}
+
+	return sqlx.Open("postgres", u.String())
+}
